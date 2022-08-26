b2d661d3d42f13b52995e9650124d65ad370c70a
Will Kee <william.w.kee@gmail.com>
2022-08-25 21:56:36 -0600

    fixed error in models index.js file

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/db.js")[env];
+const config = require(__dirname + "/../../config/db.js")[env];
 const db = {};
 
 let sequelize;
@@ -29,7 +29,10 @@ fs.readdirSync(__dirname)
 		);
 	})
 	.forEach((file) => {
-		const model = sequelize["import"](path.join(__dirname, file));
+		const model = require(path.join(__dirname, file))(
+			sequelize,
+			Sequelize.DataTypes
+		);
 		db[model.name] = model;
 	});
 
