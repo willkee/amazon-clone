81db5298f98a99bb6d5dd3715660490927bffd54
Will Kee <william.w.kee@gmail.com>
2022-09-09 21:40:49 -0600

    added products reducer to store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,8 +3,9 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 import sessionReducer from "./session";
+import productsReducer from "./products";
 
-const reducer = { session: sessionReducer };
+const reducer = { session: sessionReducer, products: productsReducer };
 
 const store = configureStore({
 	reducer,
