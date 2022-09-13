e655293fbb456dddbe84a7bf7f7cc8600e8452ed
Will Kee <william.w.kee@gmail.com>
2022-09-12 20:54:10 -0600

    added second product

--- backend/db/seeders/2-products-seed.js
+++ backend/db/seeders/2-products-seed.js
@@ -15,6 +15,16 @@ module.exports = {
 						"https://www.candywarehouse.com/item-images/130818-01_giant-red-gummy-bear.jpg",
 					price: 99.99,
 				},
+				{
+					sellerId: 1,
+					name: "Boeing 747",
+					shortDesc: "This is a very large airplane.",
+					longDesc:
+						"This is a double decker airplane and can hold hundreds of people for very long flights.",
+					imageUrl:
+						"https://upload.wikimedia.org/wikipedia/commons/3/33/Boeing_747-8_first_flight_Everett%2C_WA.jpg",
+					price: 9999.99,
+				},
 			],
 			{}
 		);
