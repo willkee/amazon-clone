5066d010dfd6e1de20d1c92e6ff2b63413de8f0b
Will Kee <william.w.kee@gmail.com>
2022-09-12 20:49:03 -0600

    showing products on frontend

---
+++ frontend/src/components/Products/AllProducts.js
@@ -0,0 +1,18 @@
+import styles from "./Products.module.css";
+
+export default function AllProducts({ products }) {
+	return (
+		<div>
+			<div>
+				{products.map((product) => (
+					<div className={styles.product_container} key={product.id}>
+						<h3>{product.name}</h3>
+						<div>{product.shortDesc}</div>
+						<div>${product.price}</div>
+						<img src={product.imageUrl} alt="product img"></img>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,11 +1,13 @@
 import Nav from "./Navigation";
 import Login from "./Login";
 import Home from "./Homepage";
+import AllProducts from "./Products/AllProducts";
 
 const Component = {
 	Nav,
 	Login,
 	Home,
+	AllProducts,
 };
 
 export default Component;
